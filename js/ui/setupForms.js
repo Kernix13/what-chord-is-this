f2895fc8558b519d4933384468af3a8c66d4fb32
Jim <jimkernicky@gmail.com>
2025-09-21 08:13:13 -0400

    create setupForms.js and loadTunings Fx

---
+++ js/ui/setupForms.js
@@ -0,0 +1,25 @@
+import { TUNINGS } from "../data/constants.js";
+import { getLocalStorage } from "../utils/storage.js";
+
+const tuningsSelect = document.getElementById('tunings-select');
+const stringsDiv = document.getElementById('strings');
+
+// Load instruments tunings into the DOM (Settings Form)
+export function loadTunings(index) {
+  const instrumentTunings = TUNINGS[index];
+
+  let i = 0;
+
+  for (const tuning in instrumentTunings) {
+    const option = document.createElement('option');
+    option.setAttribute('value', i);
+    i++;
+
+    // Stringify the tunings arrays then append to the DOM
+    const tuningString = instrumentTunings[tuning].join('-');
+    const optionText = document.createTextNode(`${tuning}: ${tuningString}`);
+
+    option.append(optionText);
+    tuningsSelect.append(option);
+  }
+}
