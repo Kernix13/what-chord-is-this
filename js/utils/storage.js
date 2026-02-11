efed80e3ca92cfe0cfe417cb4bb1ec7d3dc89046
Jim <jimkernicky@gmail.com>
2026-02-11 14:54:45 -0500

    edit storage functions

--- js/utils/storage.js
+++ js/utils/storage.js
@@ -1,7 +1,7 @@
-export function setLocalStorage(str, obj) {
-  localStorage.setItem(str, JSON.stringify(obj));
+export function setLocalStorage(key, obj) {
+  localStorage.setItem(key, JSON.stringify(obj));
 }
 
-export function getLocalStorage(str) {
-  return JSON.parse(localStorage.getItem(str));
+export function getLocalStorage(key) {
+  return JSON.parse(localStorage.getItem(key));
 }
